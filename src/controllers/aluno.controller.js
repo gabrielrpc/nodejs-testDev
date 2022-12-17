37e9616247dce87602c1d067794060832b7795b1
Gabriel Carvalho <grpc15@outlook.com>
2022-12-16 23:00:59 -0300

    funcao add json

--- src/controllers/aluno.controller.js
+++ src/controllers/aluno.controller.js
@@ -1,18 +1,26 @@
 const Aluno = require("../models/aluno.model");
-const fileSy = require("../functions/index")
-const file = "./alunos.json"
+const fs = require("fs");
+const { JSONRead } = require("../functions");
+const filePath = "./alunos.json";
 
 module.exports = {
   async create(req, res) {
     const { nome_aluno, email_aluno, data_nasc_aluno, matricula_aluno } =
       req.body;
-    let data = {};
+    let newData = {};
     let user = await Aluno.findOne({ email_aluno });
     if (!user) {
-      data = { nome_aluno, email_aluno, data_nasc_aluno, matricula_aluno };
-      user = await Aluno.create(data);
-      const userData = await Aluno.find()
-      await fileSy.JSONUpdate(file, userData);
+     function addNewItem(err, data) {
+        if (err) console.log("error", err);
+        let json = JSON.parse(data);
+        json.data.push(newData);
+        fs.writeFile(filePath, JSON.stringify(json, null, 2), (err, result) => {
+          if (err) console.log("error", err);
+        });
+      }
+      newData = { nome_aluno, email_aluno, data_nasc_aluno, matricula_aluno };
+      fs.readFile(filePath, addNewItem);
+      user = await Aluno.create(newData);
       return res.status(200).json(user);
     } else {
       return res.status(500).json(user);
@@ -20,7 +28,8 @@ module.exports = {
   },
 
   async listar(req, res) {
+    // const user = await JSONRead(filePath)
     const user = await Aluno.find()
-      res.json(user);
-  }
+    res.json(user);
+  },
 };
--- src/functions/index.js
+++ src/functions/index.js
@@ -1,18 +1,5 @@
 const fs = require("fs");
 
-const JSONWrite = (file, data, encoding = "utf-8") => {
-  const promiseCalback = (resolve, reject) => {
-    fs.writeFile(file, JSON.stringify(data, null, 2), encoding, (err) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(true);
-    });
-  };
-  return new Promise(promiseCalback);
-};
-
 const JSONRead = (file, encoding = "utf-8") => {
   const promiseCalback = (resolve, reject) => {
     fs.readFile(file, encoding, (err, data) => {
@@ -31,19 +18,5 @@ const JSONRead = (file, encoding = "utf-8") => {
   return new Promise(promiseCalback);
 };
 
-const JSONUpdate = (file, newData, encoding = "utf-8") => {
-  const promiseCalback = async (resolve, rejects) => {
-    try {
-      const data = await JSONRead(file, encoding);
-      const result = { ...data, ...newData };
-      await JSONWrite(file, result, encoding);
-      resolve(result);
-    } catch (e) {
-      rejects(e);
-    }
-  };
 
-  return new Promise(promiseCalback);
-};
-
-module.exports = {JSONRead, JSONUpdate, JSONWrite}
+module.exports = {JSONRead}
